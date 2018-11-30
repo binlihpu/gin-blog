5c271df0bf381fe4ba29a050d532f833b28942c7
binlihpu <binlihpu@gmail.com>
2018-11-30 16:35:16 +0800

    add jwt

---
+++ pkg/util/jwt.go
@@ -0,0 +1,53 @@
+package util
+
+import (
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+
+	"github.com/binlihpu/gin-blog/pkg/setting"
+)
+
+var jwtSecret = []byte(setting.AppConf.JwtSecret)
+
+// Claims Claims
+type Claims struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	jwt.StandardClaims
+}
+
+// GenerateToken GenerateToken
+func GenerateToken(username, password string) (string, error) {
+	nowTime := time.Now()
+	expireTime := nowTime.Add(3 * time.Hour)
+
+	claims := Claims{
+		username,
+		password,
+		jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    "gin-blog",
+		},
+	}
+
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString(jwtSecret)
+
+	return token, err
+}
+
+// ParseToken ParseToken
+func ParseToken(token string) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+
+	return nil, err
+}
